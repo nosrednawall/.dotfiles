2f2240b2765cb21e47fbe3ddc53ef1fceee70f78
nosrednawall <nosrednawall@gmail.com>
2020-12-17 22:41:33 -0300

    add wallpapers

--- debian-bspwm/arquivos/bspwm/startup.sh
+++ debian-bspwm/arquivos/bspwm/startup.sh
@@ -9,5 +9,5 @@ if [[ $MON == 2 ]] ; then
         xrandr --output VGA-1 --mode 1600x900 --pos 0x180 --rotate normal --output DVI-D-1 --off --output HDMI-1 --mode 2560x1080 --pos 1600x0 --rotate normal
 fi
 
-feh --bg-fill --randomize -r /home/$USER/.dotfiles/wallpapers/*
-
+#feh --bg-fill --randomize -r /home/$USER/.dotfiles/wallpapers/*
+feh --bg-fill -r /home/$USER/.dotfiles/wallpapers/vaporwave-neon-genesis.jpg
