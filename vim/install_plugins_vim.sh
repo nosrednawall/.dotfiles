0b8c4464088be9ae11d710275a139c0b37cfe622
nosrednawall <nosrednawall@gmail.com>
2020-12-25 11:54:16 -0300

    add vim configs

--- vim/install_plugins_vim.sh
+++ vim/install_plugins_vim.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-
+PATH_PADRAO="$(pwd)"
 #primeiro instala o vim
-sudo apt install vim git 7z
+sudo apt install vim git 7z python3-pip
+sudo pip3 install flake8 pylint
+sudo pip3 install jedi-language-server
+sudo npm install -g eslint
 
 # Cria as pastas
 mkdir -p ~/.vim/pack/git-plugins/start
@@ -14,18 +17,38 @@ git clone https://github.com/tomasiser/vim-code-dark
 git clone https://github.com/rafi/awesome-vim-colorschemes
 git clone https://github.com/mhinz/vim-startify
 git clone https://github.com/ryanoasis/vim-devicons
+git clone https://github.com/vim-airline/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes
+git clone https://github.com/ctrlpvim/ctrlp.vim
+git clone https://github.com/preservim/nerdcommenter
+git clone https://github.com/mattn/emmet-vim
+git clone https://github.com/dense-analysis/ale
+git clone https://github.com/sheerun/vim-polyglot
 
+mkdir -p ~/.vim/pack/coc/start
+cd ~/.vim/pack/coc/start
+curl --fail -L https://github.com/neoclide/coc.nvim/archive/release.tar.gz|tar xzfv -
+
+# For coc-java
+# mkdir -p ~/.vim/pack/coc/java
+#cd ../java
+#wget http://download.eclipse.org/jdtls/snapshots/jdt-language-server-latest.tar.gz
+
+cd $PATH_PADRAO
+ln -s coc.nvimrc ~/.vim/coc.nvimrc
 
 # Faz link sinbolico do vimrc
 ln -s vimrc ~/.vimrc
 
 # Instala a font usada
-mkdir -p ~/.fonts
-cd ~/.fonts
-wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete%20Mono.otf
+mkdir -p /home/$USER/.local/share/fonts
+cd /home/$USER/.local/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FantasqueSansMono.zip
+7z e FantasqueSansMono.zip
 fc-cache
-# Passos para JetBrainsMono
-#wget https://download.jetbrains.com/fonts/JetBrainsMono-1.0.0.zip
-#7z e JetBrainsMono-1.0.0.zip
-#rm JetBrainsMono-1.0.0.zip
-#fc-cache
+
+# Instale as extencoes de linguagens que eu uso
+# Abra o VIM
+# E Digite o comando abaixo
+:CocInstall coc-tsserver coc-json coc-html coc-css coc-python coc-jedi coc-sh coc-snippets coc-git coc-java 
+
