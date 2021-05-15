663ad0448da7b89908ecd3b9caae3f2263b024f9
Anderson José de Souza Inácio <nosrednawall@gmail.com>
2021-05-15 14:14:39 -0300

    backup is new

--- i3/arquivos/i3/restart-i3.sh
+++ i3/arquivos/i3/restart-i3.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 i3 restart
-sh ~/.config/polybar/launch.sh
+#sh ~/.config/polybar/launch.sh
+bash ~/.config/polybar/launch.sh --hack
 feh --bg-fill --randomize -r /home/$USER/.wallpapers/*
